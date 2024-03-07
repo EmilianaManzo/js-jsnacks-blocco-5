1265b2a3a9a3f930981521ee32107acf5dea4e32
EmilianaManzo <emiliana.ma96@gmail.com>
2024-03-07 15:12:48 +0100

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,68 @@
+// SNACK 1
+
+const zucchine =[
+  {
+    varieta: 'nera',
+    peso : 100,
+    lunghezza: 10 
+  },
+
+  {
+    varieta: 'napoletana',
+    peso : 150,
+    lunghezza: 15 
+  },
+
+  {
+    varieta: 'romanesca',
+    peso : 120,
+    lunghezza: 12 
+  },
+
+  {
+    varieta: 'fiorentina',
+    peso : 80,
+    lunghezza: 7 
+  },
+
+  {
+    varieta: 'tonde',
+    peso : 130,
+    lunghezza: 5 
+  },
+
+  {
+    varieta: 'trombetta',
+    peso : 90,
+    lunghezza: 12
+  },
+
+  {
+    varieta: 'patisson',
+    peso : 150,
+    lunghezza: 24 
+  },
+
+  {
+    varieta: 'gialla',
+    peso : 90,
+    lunghezza: 21
+  },
+
+  {
+    varieta: 'friulana',
+    peso : 130,
+    lunghezza: 18 
+  },
+  {
+    varieta: 'crookneck',
+    peso : 150,
+    lunghezza: 19
+  }
+];
+
+const [peso, lunghezza] = zucchine; 
+const {peso, lunghezza} = zucchina;
+
+
+console.log(peso , lunghezza);
